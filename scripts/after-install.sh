9f0c21611f6e4d2024fb07752b65772d67f57885
mihirbhatt4687 <mihirbhatt4687@gmail.com>
2022-08-23 16:54:45 +0530

    added sh script

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "Running after install..."
-sudo chmod -R 777 /var/www/html
-cd /var/www/html && ng build
+# sudo chmod -R 777 /var/www/html
+# cd /var/www/html && ng build
 #sudo touch log.txt
-sudo ls -al > log.txt
+#sudo ls -al > log.txt
+
+mv ./dist/test/ ./
 
 # rm -rf ./
 # cp -R ./dist/test/ ./
